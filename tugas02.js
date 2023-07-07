414e97c943b916499fff306e5067d18886171733
Muhammad Faisal <muh.faisal572@gmail.com>
2023-07-07 11:01:58 +0700

    adding second validaton

--- tugas02.js
+++ tugas02.js
@@ -1,4 +1,4 @@
-const matematika = 80;
+const matematika = 89;
 const bahasaIndonesia = 90;
 const bahasaInggris = 89;
 const ipa = 69;
@@ -6,16 +6,20 @@ const ipa = 69;
 let total = matematika + bahasaIndonesia + bahasaInggris + ipa;
 const avrg = total / 4;
 
-console.log(`Rata-rata = ${avrg}`);
-
-if (avrg >= 90) {
-  console.log("Grade = A");
-} else if (avrg >= 80) {
-  console.log("Grade = B");
-} else if (avrg >= 70) {
-  console.log("Grade = C");
-} else if (avrg >= 60) {
-  console.log("Grade = D");
+if (typeof avrg !== "number" || isNaN(avrg)) {
+  console.log("input harus berupa number");
 } else {
-  console.log("Grade = E");
+  console.log(`Rata-rata = ${avrg}`);
+
+  if (avrg >= 90) {
+    console.log("Grade = A");
+  } else if (avrg >= 80) {
+    console.log("Grade = B");
+  } else if (avrg >= 70) {
+    console.log("Grade = C");
+  } else if (avrg >= 60) {
+    console.log("Grade = D");
+  } else {
+    console.log("Grade = E");
+  }
 }
