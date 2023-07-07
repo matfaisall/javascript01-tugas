2af4aabe4fd7a06ed80252c578e78743cb87ac1a
Muhammad Faisal <muh.faisal572@gmail.com>
2023-07-07 10:06:09 +0700

    adding validation

--- tugas02.js
+++ tugas02.js
@@ -7,15 +7,18 @@ let total = matematika + bahasaIndonesia + bahasaInggris + ipa;
 const avrg = total / 4;
 
 console.log(`Rata-rata = ${avrg}`);
-
-if (avrg >= 90) {
-  console.log("Grade = A");
-} else if (avrg >= 80) {
-  console.log("Grade = B");
-} else if (avrg >= 70) {
-  console.log("Grade = C");
-} else if (avrg >= 60) {
-  console.log("Grade = D");
+if (avrg === "number") {
+  if (avrg >= 90) {
+    console.log("Grade = A");
+  } else if (avrg >= 80) {
+    console.log("Grade = B");
+  } else if (avrg >= 70) {
+    console.log("Grade = C");
+  } else if (avrg >= 60) {
+    console.log("Grade = D");
+  } else {
+    console.log("Grade = E");
+  }
 } else {
-  console.log("Grade = E");
+  console.log("harus berupa number");
 }
