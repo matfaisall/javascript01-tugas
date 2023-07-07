015e4506ffe66bd25956b752217e8426925ec4a4
Muhammad Faisal <muh.faisal572@gmail.com>
2023-07-07 10:11:18 +0700

    kembalikan kesemula

--- tugas02.js
+++ tugas02.js
@@ -7,18 +7,15 @@ let total = matematika + bahasaIndonesia + bahasaInggris + ipa;
 const avrg = total / 4;
 
 console.log(`Rata-rata = ${avrg}`);
-if (avrg === "number") {
-  if (avrg >= 90) {
-    console.log("Grade = A");
-  } else if (avrg >= 80) {
-    console.log("Grade = B");
-  } else if (avrg >= 70) {
-    console.log("Grade = C");
-  } else if (avrg >= 60) {
-    console.log("Grade = D");
-  } else {
-    console.log("Grade = E");
-  }
+
+if (avrg >= 90) {
+  console.log("Grade = A");
+} else if (avrg >= 80) {
+  console.log("Grade = B");
+} else if (avrg >= 70) {
+  console.log("Grade = C");
+} else if (avrg >= 60) {
+  console.log("Grade = D");
 } else {
-  console.log("harus berupa number");
+  console.log("Grade = E");
 }
